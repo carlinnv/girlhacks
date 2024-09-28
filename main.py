3bb13f142072d9aaeaaab72f349fd7b39586f5d9
Carlin <carlinnv@gmail.com>
2024-09-28 13:01:09 -0400

    added new links

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ app = Flask(__name__)
 @app.route("/")
 def main(): 
     return "<p>test main</p>"
+
+
+@app.route("/discussion")
+def discussion(): 
+    return "Discussion page"
+
+@app.route("/events")
+def events(): 
+    return "Events page"
+
+@app.route("/about")
+def about(): 
+    return "About page" 
+    
