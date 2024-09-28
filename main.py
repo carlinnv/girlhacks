3b85e1e04320acb588a2148c090eae7938ee8399
Carlin <carlinnv@gmail.com>
2024-09-28 15:20:09 -0400

    added db setup stuff

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template, request
+import os 
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column 
 
 ##database setup
 class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///project.db"
 
 db.init_app(app)
 
+##create database for discussion posts
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    caption = db.Column(db.String, nullable=False)
 
+    def __repr(self):
+        return f'<Title: {title}>'
 
+#App routing
 @app.route("/")
 def main(): 
     return "<p>test main</p>"
