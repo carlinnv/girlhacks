254ee874aa2e397c7d6dd622cd6d141128acaddb
Carlin <carlinnv@gmail.com>
2024-09-28 20:07:38 -0400

    added functional sign in
    only "admin" and "admin" work right now

--- main.py
+++ main.py
@@ -33,9 +33,15 @@ with app.app_context():
 
 
 #App routing
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def main(): 
-    return render_template("index.html")
+    error = None
+    if request.method== 'POST': 
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid'
+        else: 
+            return redirect(url_for('discussion'))
+    return render_template("index.html", error=error)
 
 
 @app.route("/discussion", methods=['GET', 'POST'])
